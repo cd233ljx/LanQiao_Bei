6d36429f492ab269c3f736d4fd954602c4b4fdab
Utopiacd <2724878969@qq.com>
2026-01-22 20:14:36 +0800

    前缀和

--- 26.1.22/P1115.cpp
+++ 26.1.22/P1115.cpp
@@ -10,19 +10,16 @@ int main(){
     int MAX_SUM = INT_MIN;
     for (int i = 1; i <= n;i++){
         cin >> a[i];
-    }
-
-    for (int i = 1; i <= n;i++){
         if (sum[i - 1] < 0)
         {
             sum[i] = a[i];
         }
         else
         {
-            sum[i] = sum[i-1] + a[i];
+            sum[i] = sum[i - 1] + a[i];
         }
 
-        MAX_SUM = max(sum[i],MAX_SUM);
+        MAX_SUM = max(sum[i], MAX_SUM);
     }
     
     cout << MAX_SUM;

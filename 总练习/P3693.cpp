b0732575329019b82561de5dee2eebf7fcdd81bc
Utopiacd <2724878969@qq.com>
2026-03-23 13:05:27 +0800

    P3693

--- 总练习/P3693.cpp
+++ 总练习/P3693.cpp
@@ -1,37 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef unsigned long long LL;
 
-const int N = 1e3 + 10;
 int T;
-LL a[N][N];
-LL sum[N][N];
+LL tmp;
+vector<vector<LL>> sum;
 
 int main()
 {
+    ios ::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     cin >> T;
-    while(T--)
+    while (T--)
     {
         LL n, m, q;
         cin >> n >> m >> q;
-        for (int i = 1; i <= n;i++)
-        {
-            for (int j = 1; j <= m;j++)
-            {
-                cin >> a[i][j];
-                sum[i][j] = sum[i][j - 1] + a[i][j] + sum[i-1][j] - sum[i-1][j-1];
-            }
-        }
-        
-        
+
+        sum.assign(n + 1, vector<LL>(m + 1, 0));
+
         for (int i = 1; i <= n; i++)
         {
             for (int j = 1; j <= m; j++)
             {
-                cout << sum[i][j] << " ";
+                cin >> tmp;
+                sum[i][j] = sum[i][j - 1] + tmp + sum[i - 1][j] - sum[i - 1][j - 1];
             }
-            cout << "\n";
         }
+
+        int u, v, x, y;
+        LL ret = 0;
+        while (q--)
+        {
+            cin >> u >> v >> x >> y;
+            ret ^= sum[x][y] - sum[x][v - 1] - sum[u - 1][y] + sum[u - 1][v - 1];
+        }
+
+        cout << ret << "\n";
+        sum.clear();
     }
 
     return 0;
